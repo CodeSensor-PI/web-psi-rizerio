44f1c00f78804ddfddbb59c084f0b7fd0e601f09
lucasomsilva <lucas.msilva@sptech.school>
2025-03-21 09:54:07 -0300

    :sparkles: feat: Importando componentes iniciais

---
+++ web-psi-rizerio/src/assets/LandingPage.jsx
@@ -0,0 +1,36 @@
+import CardComponent from "./components/cards/CardComponent"
+import Abordagem from "./pages/landingPage/abordagem/Abordagem"
+import Banner from "./pages/landingPage/banner/Banner"
+import Carreira from "./pages/landingPage/carreira/Carreira"
+import Faq from "./pages/landingPage/faq/Faq"
+import Footer from "./pages/landingPage/footer/Footer"
+import Header from "./pages/landingPage/header/Header"
+import Metodos from "./pages/landingPage/metodos/Metodos"
+import Planos from "./pages/landingPage/planos/Planos"
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Header></Header>
+
+            {/* <Banner></Banner>
+
+            <Carreira></Carreira>
+
+            <Metodos>
+                <CardComponent></CardComponent>
+            </Metodos>
+
+            <Abordagem></Abordagem>
+
+            <Planos></Planos>
+
+            <Faq></Faq>
+
+            <Footer></Footer>             */}
+
+        </div>
+    )
+}
+
+export default LandingPage
