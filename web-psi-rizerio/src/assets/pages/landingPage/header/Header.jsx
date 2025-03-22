7b89f5c091fc0a73224a994df5cf3a79a0a40162
lucasomsilva <lucas.msilva@sptech.school>
2025-03-21 23:02:31 -0300

    :sparkles: feat: Finalizando Header

--- web-psi-rizerio/src/assets/pages/landingPage/header/Header.jsx
+++ web-psi-rizerio/src/assets/pages/landingPage/header/Header.jsx
@@ -1,11 +1,27 @@
-import './header.css'
+import Botao from '../../../components/botoes/Botao';
+import styles from '../header/header.module.css'
+import Logo from '../../../images/SUBLOGO-04.png'
 
 const Header = () => {
     return (
-        <header className="header" id="header">
-            <h2>Componente da Header</h2>
+        <header className={styles.header} id="header">
+            <img src={Logo} alt="Logo JR"  id='logo_header' />
+            <navbar className={styles.navbar} id='navbar'>
+                <ul>
+                    <li><a className='navbar-item' href="#home">Home</a></li>
+                    <li><a className='navbar-item' href="#sobre">Sobre a PsiRizerio</a></li>
+                    <li><a className='navbar-item' href="#metodologias">Metodologias</a></li>
+                    <li><a className='navbar-item' href="#planos">Planos</a></li>
+                    <li><a className='navbar-item' href="#avaliacoes">Avaliações</a></li>
+                    <li><a className='navbar-item' href="#faq">FAQ</a></li>
+                </ul>
+            </navbar>
+            <div className={styles.botoes} id='botoes'>
+                <Botao
+                texto="Entrar"/>
+            </div>
         </header>
-    )
+    );
 }
 
 export default Header
