7ba8a3908101cb38b99827e4f571a23e07e5f8f7
lucasomsilva <lucas.msilva@sptech.school>
2025-03-25 08:42:14 -0300

    :sparkles: feature: Adicionando sessão de Carreira

---
+++ web-psi-rizerio/src/assets/components/titulo/TituloComponent.jsx
@@ -0,0 +1,15 @@
+import styles from '../titulo/titulo.module.css'
+
+function Titulo({ titulo, subtitulo }) {
+    return (<div className={styles.component}>
+        <div>
+            <h2 className={styles.titulo}>{titulo}</h2>
+        </div>
+        <div>
+            <h2 className={styles.subtitulo}>{subtitulo}</h2>
+        </div>
+    </div>
+    )
+}
+
+export default Titulo;
--- web-psi-rizerio/src/assets/pages/landingPage/carreira/Carreira.jsx
+++ web-psi-rizerio/src/assets/pages/landingPage/carreira/Carreira.jsx
@@ -1,8 +1,34 @@
+import InfoBox from '../../../components/infoBox/InfoBoxComponent'
+import Titulo from '../../../components/titulo/TituloComponent'
+import styles from '../carreira/carreira.module.css'
+import icone1 from '../../../../assets/react.svg'
+
 const Carreira = () => {
     return (
-        <div>
-            <h2>Componente da Carreira</h2>
-        </div>
+        <section className={styles.section}>
+            <Titulo
+                titulo="SOBRE A PSIRIZERIO"
+                subtitulo="Carreira"
+            />
+            <div className={styles.cards}>
+                <InfoBox
+                    icone={icone1}
+                    descricao="Id eros pellentesque facilisi id mollis faucibus commodo enim."
+                />
+                <InfoBox
+                    icone={icone1}
+                    descricao="Id eros pellentesque facilisi id mollis faucibus commodo enim."
+                />
+                <InfoBox
+                    icone={icone1}
+                    descricao="Id eros pellentesque facilisi id mollis faucibus commodo enim."
+                />
+                <InfoBox
+                    icone={icone1}
+                    descricao="Id eros pellentesque facilisi id mollis faucibus commodo enim."
+                />
+            </div>
+        </section>
     )
 }
 
