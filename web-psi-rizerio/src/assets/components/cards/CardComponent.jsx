9f854e8c3f04ee72841ea2c60ee03638a178fadb
lucasomsilva <lucas.msilva@sptech.school>
2025-03-25 16:31:22 -0300

    :sparkles: feat: Adicionando sessão de metodologias e criando componente de card

--- web-psi-rizerio/src/assets/components/cards/CardComponent.jsx
+++ web-psi-rizerio/src/assets/components/cards/CardComponent.jsx
@@ -1,9 +1,22 @@
-const CardComponent = () => {
+import Botao from '../botoes/BotaoComponent'
+import styles from '../cards/card.module.css'
+
+function Card() {
     return (
-        <div>
-            <h2>Componente dos Cards</h2>
-        </div>
+        <article className={styles.card}>
+            <img src="https://placehold.co/500x250" alt="Imagem de descrição do card" />
+            <div className={styles.descricao_Card}>
+                <h3 className='font-semibold'>Title</h3>
+                <span className='font-semibold'>Egestas elit dui scelerisque ut eu purus aliquam vitae habitasse.</span>
+            </div>
+            <div className={styles.botao}>
+                <Botao
+                    texto="Saiba Mais" 
+                    backgroundcolor="none"
+                />
+            </div>
+        </article>
     )
 }
 
-export default CardComponent
+export default Card
--- web-psi-rizerio/src/assets/pages/landingPage/metodos/Metodos.jsx
+++ web-psi-rizerio/src/assets/pages/landingPage/metodos/Metodos.jsx
@@ -1,8 +1,31 @@
+import styles from '../metodos/metodos.module.css'
+import Titulo from '../../../components/titulo/TituloComponent'
+import Botao from '../../../components/botoes/BotaoComponent'
+import Card from '../../../components/cards/CardComponent'
+
 const Metodos = () => {
     return (
-        <div>
-            <h2>Componente dos Métodos</h2>
-        </div>
+        <section className={styles.metodos}>
+
+            <Titulo
+                titulo="METODOLOGIAS"
+                subtitulo="Maneiras que utilizaremos nas sessões"
+            />
+
+            <div className={styles.cards}>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+
+            <div className="w-[14%]">
+                <Botao
+                    texto="Entre em contato"
+                    width="200px"
+                />
+            </div>
+
+        </section>
     )
 }
 
