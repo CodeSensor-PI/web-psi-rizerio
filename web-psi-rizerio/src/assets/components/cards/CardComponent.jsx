29b657c862414b27be907efb680cb8684c8e1654
lucasomsilva <lucas.msilva@sptech.school>
2025-03-26 09:50:23 -0300

    :recycle: refactor: Importando componente de botão e implementando no card

--- web-psi-rizerio/src/assets/components/cards/CardComponent.jsx
+++ web-psi-rizerio/src/assets/components/cards/CardComponent.jsx
@@ -1,18 +1,20 @@
-import Botao from '../botoes/BotaoComponent'
+import BotaoIcone from '../botoesIcone/botoesIconeComponent'
 import styles from '../cards/card.module.css'
+import { FaArrowRight } from 'react-icons/fa'
 
-function Card() {
+function Card(props) {
     return (
         <article className={styles.card}>
             <img src="https://placehold.co/500x250" alt="Imagem de descrição do card" />
             <div className={styles.descricao_Card}>
-                <h3 className='font-semibold'>Title</h3>
-                <span className='font-semibold'>Egestas elit dui scelerisque ut eu purus aliquam vitae habitasse.</span>
+                <h3 className='font-semibold'>{props.titulo}</h3>
+                <span className='font-semibold'>{props.descricao}</span>
             </div>
             <div className={styles.botao}>
-                <Botao
-                    texto="Saiba Mais" 
-                    backgroundcolor="none"
+                <BotaoIcone
+                    texto="Saiba Mais"
+                    hoverTextColor="var(--LightPurplePsi)"
+                    Icon={FaArrowRight}
                 />
             </div>
         </article>
