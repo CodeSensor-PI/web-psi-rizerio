a8efce85289155af6d0189404cf6e5b5376c3263
lucasomsilva <lucas.msilva@sptech.school>
2025-03-31 14:41:57 -0300

    :sparkles: feat: Adicinando sessão de FAQ e importando na landing page

--- web-psi-rizerio/src/assets/LandingPage.jsx
+++ web-psi-rizerio/src/assets/LandingPage.jsx
@@ -2,6 +2,7 @@ import Abordagem from "./pages/landingPage/abordagem/Abordagem"
 import Avaliacao from "./pages/landingPage/avaliacao/Avaliacao"
 import Banner from "./pages/landingPage/banner/Banner"
 import Carreira from "./pages/landingPage/carreira/Carreira"
+import Faq from "./pages/landingPage/faq/Faq"
 import Header from "./pages/landingPage/header/Header"
 import Metodos from "./pages/landingPage/metodos/Metodos"
 import Planos from "./pages/landingPage/planos/Planos"
@@ -16,6 +17,7 @@ const LandingPage = () => {
             <Abordagem/>
             <Planos/>
             <Avaliacao/>
+            <Faq/>
         </div>
     )
 }
--- web-psi-rizerio/src/assets/pages/landingPage/faq/Faq.jsx
+++ web-psi-rizerio/src/assets/pages/landingPage/faq/Faq.jsx
@@ -1,8 +1,53 @@
+import Accordion from '../../../components/accordion/AccordionComponent'
+import Titulo from '../../../components/titulo/TituloComponent'
+import styles from '../faq/faq.module.css'
+
 const Faq = () => {
+    const dadosFaq = [
+        {
+            id: 1,
+            pergunta: "O que é psicoterapia Online?",
+            resposta: "Psicoterapia online é um atendimento psicológico realizado pela internet."
+        },
+        {
+            id: 2,
+            pergunta: "Como funcionam as sessões?",
+            resposta: "As sessões são realizadas por videochamada, com duração média de 50 minutos."
+        },
+        {
+            id: 3,
+            pergunta: "O que eu preciso no dia do agendamento?",
+            resposta: "Você precisa de um local tranquilo e uma conexão estável à internet."
+        },
+        {
+            id: 4,
+            pergunta: "O que eu preciso no dia do agendamento?",
+            resposta: "Você precisa de um local tranquilo e uma conexão estável à internet."
+        },
+        {
+            id: 5,
+            pergunta: "O que eu preciso no dia do agendamento?",
+            resposta: "Você precisa de um local tranquilo e uma conexão estável à internet."
+        }
+    ];
+
     return (
-        <div>
-            <h2>Componente do FAQ</h2>
-        </div>
+        <section id='faq' className={styles.section_faq}>
+            <Titulo
+                titulo="FAQ"
+                subtitulo="Perguntas Frequentes"
+            />
+
+            <div className={styles.acordeon}>
+                {dadosFaq.map((item) => (
+                    <Accordion
+                        key={item.id}
+                        pergunta={item.pergunta}
+                        resposta={item.resposta}
+                    />
+                ))}
+            </div>
+        </section>
     )
 }
 
