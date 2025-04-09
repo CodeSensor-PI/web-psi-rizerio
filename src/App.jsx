85bc4d026154c589a889f7be017479eaa3dd2559
JoaoV004 <joao.osilva@sptech.school>
2025-04-09 20:19:13 -0300

    feat: tela de alterar senha e alteracao no componente do input

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DadosPessoais from './pages/sessao/formularioCadastro/step1/DadosPessoais';
 import Login from './pages/login/LoginComponent'
+import AlterarSenha from './pages/dashboard/alterarSenha/alterarSenha';
 import LandingPage from './pages/landingPage/LandingPage'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             { <Route path='/' element={<LandingPage/>} />}
             { <Route path='/login' element={<Login/>} /> }
             <Route path='/forms' element={<DadosPessoais/>} />
+            {<Route path='/alterarSenha' element={<AlterarSenha/>} /> }
           </Routes>
         </BrowserRouter>
       </div>
