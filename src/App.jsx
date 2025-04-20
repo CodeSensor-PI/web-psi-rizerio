4668e7cf3c1d25d0e740d46e66bf5a5bb4f163da
lucasomsilva <lucas.msilva@sptech.school>
2025-04-20 13:02:56 -0300

    :bug: fix: Arrumando bug de redirecionamento

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
           <Routes>
             <Route path='/' element={<LandingPage/>} />
             <Route path='/login' element={<Login/>} /> 
-            <Route path='/dashboard' element={<Dashboard/>} />
+            <Route path='/dashboard/*' element={<Dashboard/>} />
           </Routes>
         </BrowserRouter>
       </div>
