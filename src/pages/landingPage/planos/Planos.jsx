f225c0fb5f3d0dc2cc4d409b0705f83fcb822649
KauanPaschoal <kauan.psantos@sptech.school>
2025-04-24 23:02:09 -0300

    feat: Implementando busca de planos via axios com descrições fixas dos planos

--- src/pages/landingPage/planos/Planos.jsx
+++ src/pages/landingPage/planos/Planos.jsx
@@ -1,34 +1,68 @@
-import styles from './planos.module.css'
-import Titulo from "../../../components/titulo/TituloComponent"
-import CardPlanos from '../../../components/cardPlanos/cardPlanosComponent'
+import { useEffect, useState } from "react";
+import styles from "./planos.module.css";
+import Titulo from "../../../components/titulo/TituloComponent";
+import CardPlanos from "../../../components/cardPlanos/cardPlanosComponent";
+import { buscarPlanos } from "../../../provider/api";
 
 const Planos = () => {
-    return (
-        <>
-            <section className={styles.section_planos} id='planos'>
-                <div className={styles.titulo_planos}>
-                    <Titulo
-                        titulo="PLANOS"
-                        subtitulo="Planos de consulta"
-                    />
-                    <span className='text-center'>Oferecemos diferentes planos de consultas para atender às suas necessidades. Cada plano é pensado para proporcionar o melhor cuidado e acompanhamento, com preços acessíveis e condições que cabem no seu bolso. Consulte abaixo os valores e escolha o plano que melhor se adapta a você.</span>
-                </div>
+  const [planos, setPlanos] = useState([]);
 
-                <div className={styles.container_planos}>
-                    <CardPlanos
-                        titulo="Avulso"
-                        descricao='Plano avulso para você que quer fazer a primeira sessão ou uma sessão de terapia individual um "test-drive"!'
-                        preco="R$ 80"
-                    />
-                    <CardPlanos
-                        titulo="Mensal"
-                        descricao="Plano mensal para você que quer ter uma rotina de terapia, onde nos encontramos em todos os meses para melhor atender você!"
-                        preco="R$ 100"
-                    />
-                </div>
-            </section>
-        </>
-    )
-}
+  useEffect(() => {
+    const fetchPlanos = async () => {
+      try {
+        const data = await buscarPlanos();
 
-export default Planos
+        const planosComDescricoesFixas = data.map((plano, index) => {
+          if (index === 0) {
+            return {
+              ...plano,
+              descricao:
+                'Plano avulso para você que quer fazer a primeira sessão ou uma sessão de terapia individual um "test-drive"!',
+            };
+          } else if (index === 1) {
+            return {
+              ...plano,
+              descricao:
+                "Plano mensal para você que quer ter uma rotina de terapia, onde nos encontramos em todos os meses para melhor atender você!",
+            };
+          }
+          return plano;
+        });
+
+        setPlanos(planosComDescricoesFixas);
+      } catch (error) {
+        console.error("Erro ao carregar os planos:", error);
+      }
+    };
+
+    fetchPlanos();
+  }, []);
+
+  return (
+    <section className={styles.section_planos} id="planos">
+      <div className={styles.titulo_planos}>
+        <Titulo titulo="PLANOS" subtitulo="Planos de consulta" />
+        <span className="text-center">
+          Oferecemos diferentes planos de consultas para atender às suas
+          necessidades. Cada plano é pensado para proporcionar o melhor cuidado
+          e acompanhamento, com preços acessíveis e condições que cabem no seu
+          bolso. Consulte abaixo os valores e escolha o plano que melhor se
+          adapta a você.
+        </span>
+      </div>
+
+      <div className={styles.container_planos}>
+        {planos.map((plano) => (
+          <CardPlanos
+            key={plano.id}
+            titulo={plano.categoria}
+            descricao={plano.descricao}
+            preco={`R$ ${plano.preco}`}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Planos;
--- src/provider/api.js
+++ src/provider/api.js
@@ -1,16 +1,25 @@
 import axios from "axios";
 
 export const buscarEnderecoPorCep = async (cep) => {
-    try {
-        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+  try {
+    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
 
-        if (response.data.erro) {
-            throw new Error('CEP não encontrado.');
-        }
-        return response.data;
-    } catch (error) {
-        console.error('Erro ao buscar o endereço:', error.message);
-        throw error;
+    if (response.data.erro) {
+      throw new Error("CEP não encontrado.");
     }
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar o endereço:", error.message);
+    throw error;
+  }
+};
 
+export const buscarPlanos = async () => {
+  try {
+    const response = await axios.get("http://localhost:8080/planos");
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar os planos:", error.message);
+    throw error;
+  }
 };
