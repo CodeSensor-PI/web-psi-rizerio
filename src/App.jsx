fb1eb6996643fa6e634379007ca6ae5faa9d596f
JoaoV004 <joao.osilva@sptech.school>
2025-04-25 22:48:34 -0300

    ✨feat: Tela de meus agendamentos

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/login/LoginComponent'
 import AlterarSenha from './pages/dashboard/alterarSenha/alterarSenha';
 import LandingPage from './pages/landingPage/LandingPage'
-import Localidade from './pages/sessao/formularioCadastro/step2/Localidade';
-import Contato from './pages/sessao/formularioCadastro/step3/Contato';
-import Conclusao from './pages/sessao/formularioCadastro/step4/Conclusao';
+import Localidade from './pages/sessao/';
+import Contato from './pages/sessao/dashboard/formularioCadastro/step3/Contato';
+import Conclusao from './pages/sessao/dashboard/formularioCadastro/step4/Conclusao';
 import MeusAgendamentos from './pages/dashboard/meusAgendamentos/meusAgendamentos';
 
 
