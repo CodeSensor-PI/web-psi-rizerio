49c10a750bfa56547702e03dcdd62011cb7e5842
lucasomsilva <lucas.msilva@sptech.school>
2025-04-29 16:19:13 -0300

    :recycle: refactor: Colocando informações nos cards de metodologias

--- src/components/cards/CardComponent.jsx
+++ src/components/cards/CardComponent.jsx
@@ -4,10 +4,10 @@ import { FaArrowRight } from 'react-icons/fa'
 function Card(props) {
     return (
         <article className={styles.card}>
-            <img src="https://placehold.co/500x250" alt="Imagem de descrição do card" />
+            <img className='h-[60%]' src={props.imagem} alt="Imagem de descrição do card" />
             <div className={styles.descricao_Card}>
                 <h3 className='font-semibold'>{props.titulo}</h3>
-                <span className='font-semibold'>{props.descricao}</span>
+                <span>{props.descricao}</span>
             </div>
             <button className={styles.botao}>
                 Saiba Mais <FaArrowRight className='w-[20%] h-[60%]'/>
--- src/pages/landingPage/metodos/Metodos.jsx
+++ src/pages/landingPage/metodos/Metodos.jsx
@@ -2,6 +2,9 @@ import styles from './metodos.module.css'
 import Titulo from '../../../components/titulo/TituloComponent'
 import Botao from '../../../components/botoes/BotaoComponent'
 import Card from '../../../components/cards/CardComponent'
+import ImagemDescritiva from '../../../assets/images/imagem-descritiva.jpg'
+import ImagemExperimental from '../../../assets/images/imagem-experimental.jpg'
+import ImagemCorrelacional from '../../../assets/images/imagem-correlacional.jpg'
 
 const Metodos = () => {
     return (
@@ -14,14 +17,17 @@ const Metodos = () => {
 
             <div className={styles.cards}>
                 <Card
+                    imagem={ImagemDescritiva}
                     titulo="Pesquisa Descritiva"
                     descricao="Busca descrever e analisar fenômenos psicológicos, muitas vezes utilizando métodos como entrevistas e observações. "
                 />
                 <Card
+                    imagem={ImagemExperimental}
                     titulo="Pesquisa Experimental"
                     descricao="Envolve a manipulação de variáveis para observar seus efeitos, permitindo estabelecer relações causais."
                 />
                 <Card
+                    imagem={ImagemCorrelacional}
                     titulo="Pesquisa Correlacional"
                     descricao="Examina a relação entre duas ou mais variáveis, identificando padrões, sem estabelecer relações causais."
                 />
