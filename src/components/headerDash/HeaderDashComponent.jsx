328cfd18d5eb12a8e89691fca9d74f59d9ce9acb
KauanPaschoal <kauan.psantos@sptech.school>
2025-04-28 21:06:14 -0300

    feat: Adicionar funcionalidade de botão voltar e melhorar o layout no componente HeaderDash

--- src/components/headerDash/HeaderDashComponent.jsx
+++ src/components/headerDash/HeaderDashComponent.jsx
@@ -1,20 +1,51 @@
-import styles from './headerDash.module.css';
+import styles from "./headerDash.module.css";
 import { BsGear } from "react-icons/bs";
 import { IoIosLogOut } from "react-icons/io";
+import { IoArrowBack } from "react-icons/io5"; // Ícone de voltar
 
-function HeaderDash({showSettingsIcon}) {
-    return (
-        <>
-            <header className={styles.header_dash}>
-                <div className={styles.icons}>
-                    {showSettingsIcon && (
-                        <BsGear title="Configurações" className={styles.icon} />
-                    )}
-                    <IoIosLogOut title="Sair da conta" className={styles.icon} />
-                </div>
-            </header>
-        </>
-    )
+function HeaderDash({ showSettingsIcon, showBackButton = false }) {
+  const handleLogout = () => {
+    localStorage.clear();
+    sessionStorage.clear();
+    window.location.href = "/login";
+  };
+
+  const handleSettings = () => {
+    window.location.href = "/dashboard/alterar-senha";
+  };
+
+  const handleBack = () => {
+    window.location.href = "/dashboard/meus-agendamentos";
+  };
+
+  return (
+    <header className={styles.header_dash}>
+      <div className={styles.leftIcons}>
+        {showBackButton && (
+          <IoArrowBack
+            title="Voltar"
+            className={styles.icon}
+            onClick={handleBack}
+          />
+        )}
+      </div>
+
+      <div className={styles.rightIcons}>
+        {showSettingsIcon && (
+          <BsGear
+            title="Configurações"
+            className={styles.icon}
+            onClick={handleSettings}
+          />
+        )}
+        <IoIosLogOut
+          title="Sair da conta"
+          className={styles.icon}
+          onClick={handleLogout}
+        />
+      </div>
+    </header>
+  );
 }
 
 export default HeaderDash;
