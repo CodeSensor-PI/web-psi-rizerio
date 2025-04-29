d2045cd710b7c3c1691c7f5281f7c70e96d83247
KauanPaschoal <kauan.psantos@sptech.school>
2025-04-29 10:22:04 -0300

    feat: adicionando configuração de proxy para o endpoint planos

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,12 @@ export default defineConfig({
         secure: false,
         rewrite: (path) => path.replace(/^\/sessoes/, "/sessoes"),
       },
+      "/planos": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/planos/, "/planos"),
+      },
     },
   },
 });
