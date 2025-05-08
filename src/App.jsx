cd7ac35f4484647f24de39101a7ffa3bd7dcdaad
lucasomsilva <lucas.msilva@sptech.school>
2025-05-08 12:35:21 -0300

    :sparkles: feat: Adicionando arquivo de rotas e alterando App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './pages/login/LoginComponent'
-import LandingPage from './pages/landingPage/LandingPage'
-import Dashboard from './pages/dashboard/Dashboard';
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './routes'
 
 function App() {
   return (
     <>
       <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<LandingPage/>} />
-            <Route path='/login' element={<Login/>} />
-            <Route path='/dashboard/*' element={<Dashboard/>} /> 
-          </Routes>
-        </BrowserRouter>
+        <RouterProvider router={routes} />
       </div>
     </>
   )
